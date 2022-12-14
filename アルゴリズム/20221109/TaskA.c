1e39af81eeb7315a6645e1d0b23112eaae823c30
watanabe1115-ux <86214361+watanabe1115-ux@users.noreply.github.com>
2022-12-14 16:08:53 +0900

    Add files via upload

---
+++ アルゴリズム/20221109/TaskA.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int target)
+{
+    if(target<=1){
+        return target;
+    }
+    else{
+        int fact=1;
+        for(int i=target;i>0;-i){
+            fact*=i;
+        }
+        return fact;
+    }
+
+}
---
+++ アルゴリズム/20221214/Stak.c
@@ -0,0 +1,22 @@
+#include <stdio,h>
+
+int factorial(int target) {
+if (target <= 1) {
+return target;
+}
+else {
+int fact = 1;
+for (int i = target; i > 0; --i) {
+fact *= i;
+}
+return fact;
+}
+}
+int factorial(int target) {
+if (target <= 1) {
+return target;
+}
+else {
+return target * factorial(target - 1);
+}
+}
